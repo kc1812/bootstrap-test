1ceac1f2521898d82f2c06d96ae65ea13763da74
Kunal Chaudhary <kunal.ch1812@gmail.com>
2019-06-05 13:58:04 +0530

    assignment 4

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    // caraousel
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+    // modal
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('toggle');
+    });
+    $("#loginLink").click(function(){
+        $("#loginModal").modal('toggle');
+    });
+});
